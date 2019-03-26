a542168e8240b80bed4f5e1290fedbcef29b2c44
lirenzuo <xxxxxxxxxxxxxx>
2019-03-26 17:33:18 +0800

    m

---
+++ src/main/java/com/imooc/jvm/test1/Test3.java
@@ -0,0 +1,35 @@
+package com.imooc.jvm.test1;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test3 {
+    private static final int _1MB = 1024 * 1024;
+
+
+    /*
+       -Xms20m
+       -Xmx40m
+       -Xmn10m
+       -XX:+PrintGCDetails
+       -XX:+PrintGCDateStamps
+       -XX:+UseSerialGC
+       */
+
+
+
+    public static void main(String[] args) throws Exception {
+        Thread.sleep(15000);
+        List<byte[]> list = new ArrayList<byte[]>();
+
+
+        for (int i = 0; i < 30; i++) {
+            list.add(new byte[_1MB]);
+            Thread.sleep(100);
+        }
+
+        Thread.sleep(5000);
+        System.gc();
+    }
+}
