ead375add67f2f39beac4c25d465c4d2893e8587
lirenzuo <xxxxxxxxxxxxxx>
2019-03-26 20:13:03 +0800

    m

---
+++ src/main/java/com/imooc/jvm/test1/Test5.java
@@ -0,0 +1,35 @@
+package com.imooc.jvm.test1;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test5 {
+    private static final int _1MB = 1024 * 1024;
+
+
+    /*
+       -Xms20m
+       -Xmx20m
+       -Xmn10m
+       -XX:+PrintGCDetails
+       -XX:+PrintGCDateStamps
+       -XX:+UseSerialGC
+       -XX:+HeapDumpOnOutOfMemoryError
+       */
+
+
+
+    public static void main(String[] args) throws Exception {
+        Thread.sleep(15000);
+        List<byte[]> list = new ArrayList<byte[]>();
+
+
+        for (int i = 0; i < 30; i++) {
+            list.add(new byte[_1MB]);
+            Thread.sleep(100);
+        }
+
+        Thread.sleep(2000);
+    }
+}
